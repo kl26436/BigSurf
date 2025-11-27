bd66d04fe6ab266ccafbb22223ea248c85dd76e8
Kevin LaPerriere <kevin.laperriere@gmail.com>
2025-11-27 18:41:39 -0500

    Bump to v3.25

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Big Surf Workout Tracker - Service Worker
 // Provides basic offline functionality and faster loading
 
-const CACHE_NAME = 'big-surf-v3.24-sign-out-persistence-fix';
+const CACHE_NAME = 'big-surf-v3.25-sign-out-timing-fix';
 const STATIC_ASSETS = [
   '/index.html',
   '/style.css',
